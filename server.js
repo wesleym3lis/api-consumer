60b08d67550ca054214c9213207b46ed5e59d514
wesleym3lis <wesley.m3lis@hotmail.com>
2023-12-12 19:30:05 -0300

    innitial commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 
 const app = express();
 const port = 3000;
+let apiKey = process.env.API_KEY.replace(/^"(.*)"$/,'$1');
 
 app.get("/", (req, res) => {
     res.render("index.ejs", { content: "API Response." });
@@ -11,12 +12,12 @@ app.get("/", (req, res) => {
 
 app.get("/weather" , async (req , res) =>{
     try{
-        //TODO: add parametros como variavel de ambiente
-        const response = await axios.get("https://api.openweathermap.org/data/2.5/weather?lat=-22.7699481&lon=-43.4217085&appid=6cc78c07724450b966c29e7ed9606026");
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=-22.7699481&lon=-43.4217085&appid=${apiKey}`);
         const cityName = response.data.name;
         const temp =  Math.floor(response.data.main.temp - 273);
         const realFeel = Math.floor(response.data.main.feels_like - 273);
         res.render("index.ejs", { cityName, temp, realFeel });
+        
         //retorna temperatura em Kelvin
         console.log("Response successful!", response.status);
     } catch (error) {

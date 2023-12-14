e7fd94c6c3c0fb88c79b328a911ae6db72f57cea
wesleym3lis <wesley.m3lis@hotmail.com>
2023-12-14 18:28:33 -0300

    Adds lat and lon as environment variables

--- server.js
+++ server.js
@@ -5,20 +5,17 @@ require("dotenv").config();
 const app = express();
 const port = 3000;
 let apiKey = process.env.API_KEY.replace(/^"(.*)"$/,'$1');
-
-app.get("/", (req, res) => {
-    res.render("index.ejs", { content: "API Response." });
-  });
+let latitude = process.env.LAT.replace(/^"(.*)"$/,'$1');
+let longitude = process.env.LON.replace(/^"(.*)"$/,'$1');
 
 app.get("/weather" , async (req , res) =>{
     try{
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=-22.7699481&lon=-43.4217085&appid=${apiKey}`);
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`);
         const cityName = response.data.name;
         const temp =  Math.floor(response.data.main.temp - 273);
         const realFeel = Math.floor(response.data.main.feels_like - 273);
         res.render("index.ejs", { cityName, temp, realFeel });
         
-        //retorna temperatura em Kelvin
         console.log("Response successful!", response.status);
     } catch (error) {
         console.error("There was a problem calling weather API", error.message);
